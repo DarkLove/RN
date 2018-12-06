1b1ff452bbc7b26e178176888731459e5677c788
fuxing <15874908437@163.com>
2018-12-06 10:55:06 +0800

    配置标题四  自定义

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text,Image } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
+
+class LogoTitle extends React.Component {
+    render() {
+        return (
+            <Image
+                source={require('./src/imags/ic_launcher.png')}
+                style={{ width: 30, height: 30 }}
+            />
+        );
+    }
+}
+
 
 class HomeScreen extends React.Component {
     static navigationOptions = {
-        title: 'Home',
+        headerTitle: <LogoTitle />,
     };
 
     render() {
