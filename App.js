b3c57729f46becb725abe5c3a6be6edc4c98d59a
fuxing <15874908437@163.com>
2018-12-06 10:01:57 +0800

    home->details

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text,Button } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 class HomeScreen extends React.Component {
@@ -7,15 +7,42 @@ class HomeScreen extends React.Component {
         return (
             <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
                 <Text>Home Screen</Text>
+                <Button
+                    title="Go to Details"
+                    onPress={() => this.props.navigation.navigate('Details')}
+                />
             </View>
         );
     }
 }
 
-const AppNavigator = createStackNavigator({
-    Home: {
-        screen: HomeScreen
-    }
-});
 
-export default createAppContainer(AppNavigator);
+class DetailsScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text>Details Screen</Text>
+            </View>
+        );
+    }
+}
+
+const RootStack = createStackNavigator(
+    {
+        Home: HomeScreen,
+        Details: DetailsScreen
+    },
+    {
+        initialRouteName: "Home"
+    }
+);
+export default createAppContainer(RootStack);
+
+
+
+// const AppNavigator = createStackNavigator({
+//     Home: {
+//         screen: HomeScreen
+//     }
+// });
+// export default createAppContainer(AppNavigator);
